31073597e91783c523dc0234090fdacabe82d5b4
Harshali-22 <harshalitrade@gmail.com>
2025-09-24 12:21:58 +0530

    no changes

--- NinzaHRMProject/src/test/java/com/NinzaHRMProject/projectTest/SampleTest1.java
+++ NinzaHRMProject/src/test/java/com/NinzaHRMProject/projectTest/SampleTest1.java
@@ -15,7 +15,7 @@ public class SampleTest1 extends BaseClass{
 	public void test1_2() throws EncryptedDocumentException, IOException {
 		System.out.println("Execute test1.2");
 		ExcelUtility eLib=new ExcelUtility();
-		System.out.println(eLib.getDataFromExcel("prod", 2, 1));
+		System.out.println(eLib.getDataFromExcel("prod", 1, 1));
 		eLib.setDataIntoExcel("prod", 6, 1, "SetValue-Working");
 		
 	}
