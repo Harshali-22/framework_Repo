eb25dde2a4c371fd67076d517276d12e9fa68b4b
Harhshali-22 <harshalitrade@gmail.com>
2025-09-25 14:32:42 +0530

    Few changes done

--- NinzaHRMProject/src/test/java/com/NinzaHRMProject/projectTest/SampleTest1.java
+++ NinzaHRMProject/src/test/java/com/NinzaHRMProject/projectTest/SampleTest1.java
@@ -3,13 +3,22 @@ package com.NinzaHRMProject.projectTest;
 import java.io.IOException;
 
 import org.apache.poi.EncryptedDocumentException;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.xml.XmlTest;
 
 import com.NinzaHRMProject.generic.baseclassutility.BaseClass;
 import com.NinzaHRMProject.generic.fileutility.ExcelUtility;
 
 public class SampleTest1 extends BaseClass{
 
+	@Test(enabled = false)
+	public void test1_3() {
+		System.out.println("Execute test1.3");
+		System.out.println(System.getProperty("browser"));
+		System.out.println(System.getProperty("url"));
+		System.out.println(System.getProperty("suite"));
+	}
 	
 	@Test(groups="Regression")
 	public void test1_2() throws EncryptedDocumentException, IOException {
@@ -17,13 +26,7 @@ public class SampleTest1 extends BaseClass{
 		ExcelUtility eLib=new ExcelUtility();
 		System.out.println(eLib.getDataFromExcel("prod", 1, 1));
 		eLib.setDataIntoExcel("prod", 6, 1, "SetValue-Working");
-		
+	
 	}
-	@Test(groups="Smoke")
-	public void test1_3() {
-		System.out.println("Execute test1.3");
-		System.out.println(System.getProperty("browser"));
-		System.out.println(System.getProperty("url"));
-		System.out.println(System.getProperty("suite"));
-	}
+
 }
